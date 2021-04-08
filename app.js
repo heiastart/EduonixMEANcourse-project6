93b5e77e410bddb44c49b519e548c3ffd090f6cf
Oyvind Sorlie <40733351+heiastart@users.noreply.github.com>
2021-04-08 18:48:25 +0200

    Added code in app.js for the routes and models

--- app.js
+++ app.js
@@ -6,6 +6,24 @@ const app = express();
 // To set up the database (MongoDB) connection, we invoke the function from initDB module
 require('./initDB')();
 
+// Import the routes for all the models/objects in this app
+const endpointsAuthor = require('./router/author');
+const endpointsBook = require('./router/book');
+const endpointsGenre = require('./router/genre');
+const endpointsLibrary = require('./router/library');
+const endpointsPubCompany = require('./router/pubCompany');
+
+// Important app.use config
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use('/author', endpointsAuthor);
+app.use('/book', endpointsBook);
+app.use('/genre', endpointsGenre);
+app.use('/library', endpointsLibrary);
+app.use('/pub-company', endpointsPubCompany);
+
 
 // Testroute
 app.get('/', (req, res) => {
